327574ea16c3e5b1c065b5ad2a87ed4250164a72
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-08-02 22:28:04 +0100

    env

--- apps/astro/docs/astro.config.mjs
+++ apps/astro/docs/astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import * as dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const envPath = path.resolve(__dirname, '../../../.env');
+dotenv.config({ path: envPath });
 
 // https://astro.build/config
 export default defineConfig({
     server: {
-        port: 3001
+        port: process.env.DOCS_SERVER_URL
     }
 });
