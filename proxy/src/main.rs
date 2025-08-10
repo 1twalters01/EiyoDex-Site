996bef4a363bb1cd127f688806d28d95ce0c989b
1twalters01 <1twalters01>
2025-08-10 20:05:28 +0100

    env function

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,72 +1,18 @@
 use actix_web::{
     App,
-    HttpRequest,
-    HttpResponse,
     HttpServer,
     web,
 };
-use reqwest::{
-    Client,
-    Method,
-};
-use core::panic;
 use std::env;
 
-// async fn proxy(req: HttpRequest, body: web::Bytes, target: String) -> HttpResponse {
-async fn proxy(req: HttpRequest, body: web::Bytes, target: String, prefix: &str) -> HttpResponse {
-    let client = Client::new();
-
-    let unsplit_uri = req.uri().to_string();
-    let uri = unsplit_uri.strip_prefix(prefix).unwrap_or_default();
-    let forward_url = format!("{}{}", target, uri);
-    println!("forward url: {}", forward_url);
-
-    let req_method: Method = req.method().to_string().parse().unwrap();
-    let mut forward_req = client
-        .request(req_method, forward_url)
-        .body(body.clone());
-
-    for (name, value) in req.headers().iter() {
-        if let Ok(val_str) = value.to_str() {
-            forward_req = forward_req.header(name.as_str(), val_str);
-        }
-    }
-
-    match forward_req.send().await {
-        Ok(resp) => {
-            let status = actix_web::http::StatusCode::from_u16(resp.status().as_u16())
-                .unwrap_or(actix_web::http::StatusCode::INTERNAL_SERVER_ERROR);
-            let headers = resp.headers().clone();
-            let bytes = resp.bytes().await.unwrap_or_default();
-
-            let mut client_resp = HttpResponse::build(status);
-            for (name, value) in headers.iter() {
-                if let Ok(val_str) = value.to_str() {
-                    client_resp.append_header((name.as_str(), val_str));
-                }
-            }
-            client_resp.body(bytes)
-        }
-        Err(e) => HttpResponse::InternalServerError()
-            .body(format!("Proxy error: P{}", e)),
-    }
-}
+pub mod proxy;
+pub mod get_env;
+use get_env::get_env_path;
+use proxy::proxy;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let current_dir = env::current_dir().unwrap();
-    let workspace_dir = match current_dir.parent(){
-        Some(path) => path,
-        None => panic!("invalid directory"),
-    };
-    let workspace_env_str = workspace_dir.to_path_buf().into_os_string()
-     .into_string().unwrap() + "/.env";
-    let workspace_env = std::path::Path::new(
-        &workspace_env_str
-        );
-    let workspace_env = std::path::Path::new(".env");
-    println!("workspace env: {:?}", workspace_env);
-    dotenvy::from_path(workspace_env).expect("Failed to load .env file");
+    dotenvy::from_path(get_env_path()).expect("Failed to load .env file");
     let proxy_address = env::var("PROXY_SERVER_URL").expect("PROXY_SERVER_URL missing");
     println!("Proxy at: {:?}", proxy_address);
 
---
+++ proxy/src/proxy.rs
@@ -0,0 +1,49 @@
+use actix_web::{
+    HttpRequest,
+    HttpResponse,
+    web,
+};
+use reqwest::{
+    Client,
+    Method,
+};
+
+pub async fn proxy(req: HttpRequest, body: web::Bytes, target: String, prefix: &str) -> HttpResponse {
+    let client = Client::new();
+
+    let unsplit_uri = req.uri().to_string();
+    let uri = unsplit_uri.strip_prefix(prefix).unwrap_or_default();
+    let forward_url = format!("{}{}", target, uri);
+    println!("forward url: {}", forward_url);
+
+    let req_method: Method = req.method().to_string().parse().unwrap();
+    let mut forward_req = client
+        .request(req_method, forward_url)
+        .body(body.clone());
+
+    for (name, value) in req.headers().iter() {
+        if let Ok(val_str) = value.to_str() {
+            forward_req = forward_req.header(name.as_str(), val_str);
+        }
+    }
+
+    match forward_req.send().await {
+        Ok(resp) => {
+            let status = actix_web::http::StatusCode::from_u16(resp.status().as_u16())
+                .unwrap_or(actix_web::http::StatusCode::INTERNAL_SERVER_ERROR);
+            let headers = resp.headers().clone();
+            let bytes = resp.bytes().await.unwrap_or_default();
+
+            let mut client_resp = HttpResponse::build(status);
+            for (name, value) in headers.iter() {
+                if let Ok(val_str) = value.to_str() {
+                    client_resp.append_header((name.as_str(), val_str));
+                }
+            }
+            client_resp.body(bytes)
+        }
+        Err(e) => HttpResponse::InternalServerError()
+            .body(format!("Proxy error: P{}", e)),
+    }
+}
+
